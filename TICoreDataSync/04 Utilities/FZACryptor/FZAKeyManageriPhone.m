c2f296174d8020d1d06a29d68f07578bedc20e62
Serhey Tkachenko <stkachenko@iig-global.com>
2014-12-30 16:08:34 +0200

    Fixed stupid warning

--- TICoreDataSync/04 Utilities/FZACryptor/FZAKeyManageriPhone.m
+++ TICoreDataSync/04 Utilities/FZACryptor/FZAKeyManageriPhone.m
@@ -131,7 +131,7 @@
         if (searchResult == errSecItemNotFound) {
             TICDSLog(TICDSLogVerbosityEveryStep, @"FZACryptor iOS Key Manager didn't find a stored key");
         } else {
-            TICDSLog(TICDSLogVerbosityErrorsOnly, @"FZACryptor iOS Key Manager search error: %ld", (int)searchResult);
+            TICDSLog(TICDSLogVerbosityErrorsOnly, @"FZACryptor iOS Key Manager search error");
         }
 
         return nil;
